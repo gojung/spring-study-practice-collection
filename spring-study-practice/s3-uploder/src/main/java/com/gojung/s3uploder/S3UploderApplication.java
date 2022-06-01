f9aab224cd1f2ad7565db6cb523af6107c5dc454
gojung <wjdrndk96@gmail.com>
2022-06-01 18:24:23 +0900

    merge main

---
+++ spring-study-practice/s3-uploder/src/main/java/com/gojung/s3uploder/S3UploderApplication.java
@@ -0,0 +1,13 @@
+package com.gojung.s3uploder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3UploderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3UploderApplication.class, args);
+    }
+
+}
---
+++ spring-study-practice/s3-uploder/src/test/java/com/gojung/s3uploder/S3UploderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gojung.s3uploder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3UploderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
