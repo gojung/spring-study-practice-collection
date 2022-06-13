8c81175df40f950b477b7ccf52b47e405413ba3e
gojung <wjdrndk96@gmail.com>
2022-06-14 08:05:29 +0900

    feature: state pattern test

--- spring-study-practice/design-pattern/src/test/java/com/gojung/designpattern/state_pattern/TicketMachineTest.java
+++ spring-study-practice/design-pattern/src/test/java/com/gojung/designpattern/state_pattern/TicketMachineTest.java
@@ -1,4 +1,17 @@
+package com.gojung.designpattern.state_pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class TicketMachineTest {
-  
+
+  @Test
+  void insertCoinTest(){
+    TicketMachine ticketMachine1 = new TicketMachine();
+    ticketMachine1.insertCoin();
+  }
 }
